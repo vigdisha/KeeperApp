70721362925f20990abdf2157c38a52ffe8c4f09
vigdisha <dishavig1@gmail.com>
2022-07-22 14:58:13 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note"
+import CreateArea from "./createArea.jsx";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+ function addNote(newNotes){
+  setNotes(prevNotes =>{
+    return[...prevNotes, newNotes]
+  })
+ }
+ function deleteNote(id){
+  setNotes(prevNotes =>{
+    return prevNotes.filter((noteItem, index)=>{
+return index != id;
+    });
+  });
+ }
+  return (
+    <div>
+      <Header />
+      <CreateArea 
+        onAdd = {addNote}/>
+       { notes.map((noteItem, index) => {
+          return (<Note
+          key={index}
+          id={index}
+          tile={noteItem.title}
+          content={noteItem.content}
+          onDelete = {deleteNote}
+           />
+          )
+        })
+       }
+      {/* <Note key={1}  /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
